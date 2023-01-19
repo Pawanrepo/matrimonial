93e04758dbce88e45dca3518cdb23cffb0623478
Pawan <pawankaushikind@gmail.com>
2023-01-19 10:39:04 +0530

    imageadded

---
+++ src/layouts/footer/index.js
@@ -0,0 +1,45 @@
+import React from "react";
+import '../../css/bootstrap.min.css';
+import '../../assets/css/animate.css';
+import '../../assets/css/media.css';
+import '../../assets/css/animate.min.css';
+
+
+const Footer = () => {
+    
+  return (
+   <>
+    <footer>
+  
+ 
+
+
+    <footer class="cd-desktop">
+         <div class="container">
+            <div class="row">
+               <div class="col-lg-5">
+             <p class="text-center" style={{color: '#000000'}}>© 2022 All Rights Reserved.</p>
+            
+    </div>
+    <div class="col-lg-3">
+    <li> <a href="https://www.facebook.com" ><img src="../../public/images/facebook.png" alt="facebook"/></a> </li>
+    <li> <a href="https://twitter.com/" ><img src="../../public/images/twitter.png" alt="twitter"/></a> </li>
+    <li> <a href="https://www.linkedin.com/" ><img src="../../public/images/linkedin.png" alt="linkedin"/></a> </li>
+    <li> <a href="https://www.youtube.com/" ><img src="../../public/images/youtube.png" alt="youtube"/></a> </li>
+                       
+    </div>
+    <div class="col-lg-4">
+        <p class="text-center" style={{color: '#000000'}}>Design by Pawan Kaushik </p>
+      </div>
+     </div>
+    </div> 
+ 
+
+
+</footer>
+</footer>
+</>
+  );
+};
+
+export default Footer;
--- src/layouts/layout.js
+++ src/layouts/layout.js
@@ -1,14 +1,18 @@
 import React from "react";
+import Footer from "./footer/index";
+import { BrowserRouter } from "react-router-dom";
 
 
 
 function Layout() {
 
   return (
-   
+    <BrowserRouter forceRefresh={true}>
    <a href='www.google.com'>
       HELLLOOLLLLLLLLLL
      </a>
+     <Footer/>
+       </BrowserRouter>
   );
 }
 
